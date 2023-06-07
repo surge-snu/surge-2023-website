86639807c03cb3b9c2b461d0b487108525893da6
Aryan Tomar <aryantomar03@gmail.com>
2023-06-07 10:54:39 +0530

    Feat: Added Favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,19 @@ import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
 import "../styles/root/globals.scss";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Surge 2023</title>
+        <meta
+          name="description"
+          content="Shiv Nadar IOE's annual Sports Fest"
+        />
+        <link rel="icon" href="/Images/Favicon.png" />
+      </Head>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
