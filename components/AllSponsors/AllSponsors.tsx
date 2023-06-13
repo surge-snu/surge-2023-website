869c622f48f3a2f8390a09148d83720382612fe1
sujith-rek <ps335@snu.edu.in>
2023-06-13 20:36:01 +0530

    feat: make all sponsors component

---
+++ components/AllSponsors/AllSponsors.tsx
@@ -0,0 +1,119 @@
+import "./AllSponsors.scss";
+import Image from "next/image";
+
+type SponsImage = {
+    name : string,
+    image : string,
+    alt : string
+}
+
+function AllSponsors(){
+
+    const sponsors = [
+        {
+            name: "Stag",
+            image: "/Images/Sponsors/stag.png",
+            alt : "Stag"
+        },
+        {
+            name: "Wai Wai",
+            image: "/Images/Sponsors/waiwai.png",
+            alt : "Wai Wai"
+        },
+        {
+            name: "Dassault Systems",
+            image: "/Images/Sponsors/dassault.png",
+            alt : "Dassault Systems"
+        },
+        {
+            name: "HCL Tech",
+            image: "/Images/Sponsors/hcltech.png",
+            alt : "HCL Tech"
+        },
+        {
+            name: "Career Launcher",
+            image: "/Images/Sponsors/career.png",
+            alt : "Career Launcher"
+        },
+        {
+            name: "Levo",
+            image: "/Images/Sponsors/levo.png",
+            alt : "Levo"
+        },
+        {
+            name: "Tutle Pic",
+            image : "/Images/Sponsors/turtle.png",
+            alt : "Turtle Pic"
+        },
+        {
+            name : "Spawn Point",
+            image : "/Images/Sponsors/spawn.png",
+            alt : "Spawn Point"
+        },
+        {
+            name: "Winsome Bioceuticals",
+            image : "/Images/Sponsors/winsome.png",
+            alt : "Winsome Bioceuticals"
+        },
+        {
+            name: "Fujifilm",
+            image : "/Images/Sponsors/fujifilm.png",
+            alt : "Fujifilm"
+        },
+        {
+            name: "HCL Healthcare",
+            image : "/Images/Sponsors/hclhealth.png",
+            alt : "HCL Healthcare"
+        },
+        {
+            name: "Fisto",
+            image : "/Images/Sponsors/fisto.png",
+            alt : "Fisto"
+        },
+        {
+            name: "InHouse Digital",
+            image : "/Images/Sponsors/inhouse.png",
+            alt : "InHouse Digital"
+        },
+        {
+            name: "SNU Xplore",
+            image : "/Images/Sponsors/snuxplore.png",
+            alt : "SNU Xplore"
+        },
+        {
+            name: "Atium Sports",
+            image : "/Images/Sponsors/atium.png",
+            alt : "Atium Sports"
+        },
+        {
+            name: "BOAT",
+            image : "/Images/Sponsors/boat.png",
+            alt : "BOAT"
+        },
+        {
+            name: "Red Bull",
+            image : "/Images/Sponsors/redbull.png",
+            alt : "Red Bull"
+        }
+    ]
+
+    return(
+        <div>
+            <section className="AllSponsors">
+                <div className="AllSponsors__content">
+                    {sponsors.map((sponsor : SponsImage) => (
+                        <Image
+                            src={sponsor.image}
+                            alt={sponsor.alt}
+                            width={200}
+                            height={90}
+                            className="AllSponsors__content--card--image"
+                        />
+                    ))}
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default AllSponsors;
