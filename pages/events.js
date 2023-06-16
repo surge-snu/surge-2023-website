2279288a46e3eb4e8dc484f1f8f8743a111101dc
Ary0405 <aryansethia2003@gmail.com>
2023-06-16 22:01:30 +0530

    feat : events page and team member details updated

---
+++ pages/events.js
@@ -0,0 +1,17 @@
+import '../styles/routes/Events.scss'
+
+export default function Events() {
+    return (
+        <section className='EventsPage'>
+            <div className='EventsPage__text'>
+                <h2>Coming Soon</h2>
+                <h2>Coming Soon</h2>
+                <h2>Coming Soon</h2>
+                <h2>Coming Soon</h2>
+            </div>
+            <div className='EventsPage__imageContainer'>
+                <img className='EventsPage__imageContainer--image' src='/Images/comingSoon.png' />
+            </div>
+        </section>
+    )
+}
