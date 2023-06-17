c071099827a4a30b5a53cb56792a00d2050cbe31
Ary0405 <aryansethia2003@gmail.com>
2023-06-17 09:48:16 +0530

    fix : ui fixes for sponsor hero

--- components/PastSponsors/PastSponsors.js
+++ components/PastSponsors/PastSponsors.js
@@ -1,24 +1,32 @@
 import Link from "next/link";
 import React from "react";
 import "./PastSponsors.scss";
+import { motion } from "framer-motion";
 
 function PastSponsors() {
   return (
     <div className="PastSponsors">
-    <div className="PastSponsors__overlaybg"></div>
-    <div className="PastSponsors__overlaybg2"></div>
-    <div className="PastSposnors__content">
-          <div className="PastSponsors__content--heading">
-            <span>Past</span> Sponsors</div>
-          <div  className="PastSponsors__content--paragraph">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec cursus tellus, dapibus varius leo. Nam mattis aliquet feugiat. In tempus sed leo et imperdiet. Vivamus sollicitudin placerat vehicula. 
+      <div className="PastSponsors__overlaybg"></div>
+      <div className="PastSponsors__overlaybg2"></div>
+      <div className="PastSponsors__content">
+        <div className="PastSponsors__content--heading">
+          <span>Past</span> Sponsors
         </div>
+        <div className="PastSponsors__content--paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec cursus tellus, dapibus varius leo. Nam mattis aliquet feugiat. In tempus sed leo et imperdiet. Vivamus sollicitudin placerat vehicula.
         </div>
         <div>
-      <Link href="/PastSponsors">
-        <button className="ctabutton">Let's Talk</button>
-      </Link>
-      </div></div>
+        <motion.button
+              initial={{ opacity: 0, y: 10 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ type: "spring", stiffness: 100 }}
+              className="ctabutton"
+            >
+              <a style={{textDecoration : "none"}} href="mailto:surge@snu.edu.in">Let's Talk</a>
+            </motion.button>
+        </div>
+      </div>
+    </div>
   );
 }
 
