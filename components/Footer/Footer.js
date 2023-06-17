aee87d4e796e258fc4bcbb0a13cfbddaeb592742
Ana4506 <ag351@snu.edu.in>
2023-06-17 21:43:07 +0530

    footer updated

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,74 +6,90 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <div className="Footer">
-      <div className="Footer__logoSocials">
-        <Image
-          src="/Images/surgeLogoWhite.svg"
-          alt="Surge Logo"
-          width={200}
-          height={100}
-          className="Footer__logoSocials--logo"
-        />
-        <div className="Footer__logoSocials--socials">
-          <Link href="https://www.instagram.com/surge.snu/">
+      <div className="split-screen">
+        <div className="left-panel">
+          <div className="Footer__logoSocials">
             <Image
-              src="Images/Icons/Facebook.svg"
-              alt="Instagram Link"
-              width={30}
-              height={30}
-              className="Footer__logoSocials--socials--icon"
+              src="/Images/Surge_Logo.png"
+              alt="Surge Logo"
+              width={200}
+              height={100}
+              className="Footer__logoSocials--logo"
             />
-          </Link>
-          <Link href="https://www.instagram.com/surge.snu/">
-            <Image
-              src="Images/Icons/Linkedin.svg"
-              alt="Instagram Link"
-              width={30}
-              height={30}
-              className="Footer__logoSocials--socials--icon"
-            />
-          </Link>
-          <Link href="https://www.instagram.com/surge.snu/">
-            <Image
-              src="Images/Icons/Instagram.svg"
-              alt="Instagram Link"
-              width={30}
-              height={30}
-              className="Footer__logoSocials--socials--icon"
-            />
-          </Link>
-        </div>
-      </div>
-      <div className="Footer__links">
-        <div className="Footer__links--category">
-          <h2 className="Footer__links--category--heading">Pages</h2>
-          <div className="Footer__links--category--links">
-            <div className="Footer__links--category--links__content">
-              <Link href="/">
-                <h2>Home</h2>
+            <div className="Footer__logoSocials--socials">
+              <Link href="https://www.instagram.com/surge.snu/">
+                <Image
+                  src="Images/Facebook.svg"
+                  alt="Instagram Link"
+                  width={30}
+                  height={30}
+                  className="Footer__logoSocials--socials--icon"
+                />
               </Link>
-              <Link href="/gallery">
-                <h2>Gallery</h2>
+              <Link href="https://www.instagram.com/surge.snu/">
+                <Image
+                  src="Images/LinkedIn.svg"
+                  alt="Instagram Link"
+                  width={30}
+                  height={30}
+                  className="Footer__logoSocials--socials--icon"
+                />
               </Link>
-            </div>
-            <div className="Footer__links--category--links__content">
-              <Link href="/events">
-                <h2>Events</h2>
+              <Link href="https://www.instagram.com/surge.snu/">
+                <Image
+                  src="Images/Instagram.svg"
+                  alt="Instagram Link"
+                  width={30}
+                  height={30}
+                  className="Footer__logoSocials--socials--icon"
+                />
               </Link>
-              <Link href="/team">
-                <h2>Team</h2>
+              <Link href="https://www.instagram.com/surge.snu/">
+                <Image
+                  src="Images/Youtube.svg"
+                  alt="Instagram Link"
+                  width={30}
+                  height={30}
+                  className="Footer__logoSocials--socials--icon"
+                />
               </Link>
             </div>
           </div>
         </div>
-        <div className="Footer__links--category">
-          <h2 className="Footer__links--category--heading">Contact</h2>
-          <Link href="mailto:surge@snu.edu.in">
-            <h2>surge@snu.edu.in</h2>
-          </Link>
-          <h2>SNIoE, Delhi, Noida</h2>
-        </div>
-      </div>
+      
+        <div className="right-panel">
+          <div className="Footer__links">
+            <div className="Footer__links--category">
+              <h2 className="Footer__links--category--heading">Pages</h2>
+              <div className="Footer__links--category--links">
+                <div className="Footer__links--category--links__content">
+                  <Link href="/">
+                    <h2>Home</h2>
+                  </Link>
+                  <Link href="/gallery">
+                    <h2>Gallery</h2>
+                  </Link>
+                </div>
+                <div className="Footer__links--category--links__content">
+                  <Link href="/events">
+                    <h2>Events</h2>
+                  </Link>
+                  <Link href="/team">
+                    <h2>Team</h2>
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="Footer__links--category">
+              <h2 className="Footer__links--category--heading">Contact</h2>
+              <Link href="mailto:surge@snu.edu.in">
+                <h2>surge@snu.edu.in</h2>
+              </Link>
+              <h2>SNIoE, Delhi, Noida</h2>
+            </div>
+          </div>
+        </div>  
+      </div>  
     </div>
   );
 };
